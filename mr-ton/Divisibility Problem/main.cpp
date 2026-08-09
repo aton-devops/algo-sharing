bbd79f6a66e50d6c13c00a9615f532f425a214a9
toan-dev0 <nguyenletoanvn00@gmail.com>
2026-08-09 15:15:18 +0700

    day 2

---
+++ mr-ton/Divisibility Problem/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t > 0) {
+        int a, b;
+        cin >> a >> b;
+        cout << (b - a % b) % b << endl;
+        t--;
+    }
+    return 0;
+}
